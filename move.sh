9b060309d7894b2ddcb96fb471abcfdc23070517
maximo santoro <maximo.santoro@iovlabs.org>
2023-05-15 13:20:15 -0300

    add wallpapo

---
+++ move.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# si ya se que tengo que armar symlinks, dejame ir un poco mas de a poco. por ahora un .sh que copie los dots aca.
+
+cp ~/.config/skhd/skhdrc macos/skhdrc
+cp ~/.config/yabai/yabairc macos/yabairc
+
